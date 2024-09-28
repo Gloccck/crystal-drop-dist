8637adac6e1751bb5a69dfbd42b7d7d4156bbb53
Vladimir Kulagin <gloccck18@gmail.com>
2024-09-29 02:49:02 +0300

    Update build

---
+++ assets/tiled/tilesets/background_images-415d95e6898ccde6035dedf284652382.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="b" tilewidth="523" tileheight="428" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../textures/background/way_indicator.png" width="57" height="23"/>
+ </tile>
+ <tile id="1">
+  <image source="../../textures/background/background.png" width="523" height="428"/>
+ </tile>
+</tileset>
